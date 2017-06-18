1b7371bd2a8cdc88892882f6a5fdedc577f1e25e
Rick Buitrago <rbuitragoc@gmail.com>
2017-06-17 22:18:35 -0500

    initial checkin of a bare, console-logging express app with a pug template that reads a form text input

---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var http = require( "http" ),
+    pathUtils = require( "path" ),
+    express = require( "express" ),
+    bodyParser = require('body-parser'),
+    app = express(),
+    PORT = process.env.PORT || 5000,
+    appDir = pathUtils.resolve( __dirname, "view" );
+
+app.set('views', './view');
+app.set('view engine', 'pug');
+
+app.use( express.static( appDir ) );
+app.use(bodyParser());
+
+app.get( "*", function( req, res ) {
+    res.render('login', {} );
+} );
+
+var messages = {
+    super: "you're a super user. Be cautious\n",
+    admin: "you're an admin. Beware!\n",
+    regular: "you're a regular user. Ask your admin for more.\n"
+};
+
+app.post("*", function (req, res) {
+    var buf = "";
+    switch (req.body.user) {
+        case "super": buf += messages.super;
+        case "admin": buf += messages.admin;
+        case "regular": buf += messages.regular;
+    }
+    console.log(buf);
+});
+
+http.createServer( app ).listen( PORT, function() {
+    console.log( "Express server listening on port " + PORT );
+    console.log( "http://localhost:" + PORT );
+} );
